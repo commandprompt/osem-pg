6f4e7401a9fd883711398181a27337270f5d00d3
Jim Mlodgenski <jimm@openscg.com>
2018-01-07 07:31:06 -0500

    Fix bug on creating a promo code when adding a new sponsorship

--- app/models/sponsorship_levels_benefit.rb
+++ app/models/sponsorship_levels_benefit.rb
@@ -4,6 +4,6 @@ class SponsorshipLevelsBenefit < ActiveRecord::Base
   belongs_to :code_type
 
   def self.level_promo_codes(sponsorship)
-    SponsorshipLevelsBenefit.where(sponsorship_level_id: sponsorship.sponsorship_level_id).joins(:benefit).where("benefits.category = 1")
+    SponsorshipLevelsBenefit.where(sponsorship_level_id: sponsorship.sponsorship_level_id).joins(:benefit).where("benefits.value_type = 3")
   end
 end
