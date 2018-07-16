d73a9f55aa85db32e844511c259e0c29abf5696c
Jim Mlodgenski <jimm@openscg.com>
2018-07-16 18:58:06 -0400

    Use send_data for the ProsepectusController

--- app/controllers/prospectus_controller.rb
+++ app/controllers/prospectus_controller.rb
@@ -3,7 +3,7 @@ class ProspectusController < ApplicationController
     
   def show
     if @conference.sponsorship_info.try(:prospectus?)
-      send_file(@conference.sponsorship_info.prospectus.path,
+      send_data(@conference.sponsorship_info.prospectus.path,
                 filename:"prospectus_#{@conference.short_title}.#{@conference.sponsorship_info.prospectus.file.extension.downcase}")
     else
       redirect_to conference_path(@conference.short_title)
